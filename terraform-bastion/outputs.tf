4adae33fd0786b6efb8093144f198ceaed1f42bc
NawelDorrije <naweldorrije789@gmail.com>
2026-07-29 09:19:43 +0100

    feat(bastion): deploy bastion host and centralize secure SSH access

--- terraform-bastion/outputs.tf
+++ terraform-bastion/outputs.tf
@@ -17,3 +17,18 @@ output "ssh_lms_openedx" {
   description = "Commande SSH vers LMS-OpenedX via le Bastion"
   value       = "ssh -J ubuntu@${var.bastion_floating_ip} ubuntu@192.168.100.55"
 }
+
+output "ssh_odoo_server" {
+  description = "Commande SSH vers Odoo Server via le Bastion"
+  value       = "ssh -J ubuntu@${var.bastion_floating_ip} ubuntu@192.168.100.91"
+}
+
+output "ssh_full_stack_js" {
+  description = "Commande SSH vers Full-Stack-JS via le Bastion"
+  value       = "ssh -J ubuntu@${var.bastion_floating_ip} ubuntu@192.168.100.87"
+}
+
+output "ssh_java_js" {
+  description = "Commande SSH vers Java-JS via le Bastion"
+  value       = "ssh -J ubuntu@${var.bastion_floating_ip} ubuntu@192.168.100.149"
+}
